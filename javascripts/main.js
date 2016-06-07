e145675ab4ce3e8f3cf781d918929ec3ddf81e80
Ryan Jones <ryandjones14@gmail.com>
2016-06-07 13:20:02 -0500

    using slick plugin for images

---
+++ javascripts/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.variable-width').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+})
