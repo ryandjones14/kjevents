ba3c8361674ba0ff169a6b2653761f6ef5be1edc
Ryan Jones <ryandjones14@gmail.com>
2016-06-17 12:42:33 -0500

    got services page layout looking good

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,8 +17,12 @@ $(function(){
       var value = $(this).scrollTop();
       if ( value > 100 ) {
         $('.header').addClass('header2');
+        $('.services').addClass('services2');
+        $('.wrapper').addClass('wrapper2');
       } else {
         $('.header').removeClass('header2');
+        $('.services').removeClass('services2');
+        $('.wrapper').removeClass('wrapper2');
       }
   });
 })
