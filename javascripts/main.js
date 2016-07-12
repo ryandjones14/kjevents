9fcbe067bab66b1fee995d85b1992025c7358e6b
Ryan Jones <ryandjones14@gmail.com>
2016-07-12 14:10:36 -0500

    styles all pages for mobile

--- javascripts/main.js
+++ javascripts/main.js
@@ -15,7 +15,7 @@ $(function(){
   // Minimize header
   $(window).scroll( function() {
       var value = $(this).scrollTop();
-      if ( value > 800 ) {
+      if ( value > 1100 ) {
         $('.header').addClass('header2');
         $('.wrapper').addClass('wrapper2');
       } else {
--- javascripts/secondary.js
+++ javascripts/secondary.js
@@ -2,7 +2,7 @@ $(function(){
   // Minimize header
   $(window).scroll( function() {
       var value = $(this).scrollTop();
-      if ( value > 100 ) {
+      if ( value > 150 ) {
         $('.header').addClass('header2');
         $('.services').addClass('services2');
         $('.about').addClass('wrapper2');
