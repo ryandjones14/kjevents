1330ec864eee60aa3c3b69c52460cd6cf23c8613
Ryan Jones <ryandjones14@gmail.com>
2016-07-11 22:32:45 -0500

    edits form

--- javascripts/main.js
+++ javascripts/main.js
@@ -17,12 +17,26 @@ $(function(){
       var value = $(this).scrollTop();
       if ( value > 800 ) {
         $('.header').addClass('header2');
-        $('.services').addClass('services2');
         $('.wrapper').addClass('wrapper2');
       } else {
         $('.header').removeClass('header2');
-        $('.services').removeClass('services2');
         $('.wrapper').removeClass('wrapper2');
       }
   });
+  // var hidden;
+  // $(window).scroll( function() {
+  //     var value = $(this).scrollTop();
+  //     if ( value > 1000 ) {
+  //       $('.landing-image').css('display', 'none');
+  //       hidden = true;
+  //     }
+  //     if (hidden && value > 175){
+  //       $('.header').addClass('header2');
+  //       $('.wrapper').addClass('wrapper2');
+  //       $('.header2').animate({top: '100px'});
+  //     } else {
+  //       $('.header').removeClass('header2');
+  //       $('.wrapper').removeClass('wrapper2');
+  //     }
+  // });
 })
