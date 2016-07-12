9c65660a223c8313d5b69b6ec94232f4835a11f3
Ryan Jones <ryandjones14@gmail.com>
2016-07-12 14:36:59 -0500

    edits about Kelly section and fixes layout bug of that section

---
+++ javascripts/about.js
@@ -0,0 +1,18 @@
+$(function(){
+  // Minimize header
+  $(window).scroll( function() {
+      var value = $(this).scrollTop();
+      if ( value > 50 ) {
+        $('.header').addClass('header2');
+        $('.services').addClass('services2');
+        $('.about').addClass('wrapper2');
+        $('.form').addClass('wrapper2');
+      } else {
+        $('.header').removeClass('header2');
+        $('.services').removeClass('services2');
+        $('.wrapper').removeClass('wrapper2');
+        $('.about').removeClass('wrapper2');
+        $('.form').removeClass('wrapper2');
+      }
+  });
+})
