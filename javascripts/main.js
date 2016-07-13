76b94fa25a009b443d11f005ff16ffdb06c13383
Ryan Jones <ryandjones14@gmail.com>
2016-07-13 12:16:14 -0500

    removes commented out code

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,20 +23,4 @@ $(function(){
         $('.wrapper').removeClass('wrapper2');
       }
   });
-  // var hidden;
-  // $(window).scroll( function() {
-  //     var value = $(this).scrollTop();
-  //     if ( value > 1000 ) {
-  //       $('.landing-image').css('display', 'none');
-  //       hidden = true;
-  //     }
-  //     if (hidden && value > 175){
-  //       $('.header').addClass('header2');
-  //       $('.wrapper').addClass('wrapper2');
-  //       $('.header2').animate({top: '100px'});
-  //     } else {
-  //       $('.header').removeClass('header2');
-  //       $('.wrapper').removeClass('wrapper2');
-  //     }
-  // });
 })
